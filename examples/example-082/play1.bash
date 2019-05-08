163945364c735a856c50035ad044e51abf495aac
Vladimir Botka <vbotka@gmail.com>
2019-05-08 15:39:41 +0200

    Added example-082/play1.bash

--- examples/example-082/play1.bash
+++ examples/example-082/play1.bash
@@ -1,18 +1,6 @@
 #!/bin/bash
-sections=$(cat hosts | grep "\[")
-# echo $sections
-set -- $sections
-while [ ! -z "$2" ]
-do
-    echo "$1 $2"
-    echo "-----------------------------------------------"
-# TODO: print groups
-#    
-#   sed -e '/'"$1"'/'"$2"'/p' hosts
-#   sed -n "/webservers/,/database/p" hosts
-#   awk '/\[webservers\]/,/\[database\]/' hosts
-#   awk '/([webservers])/,/([database])/' hosts
-#   awk '/'"$1"'/,/'"$2"'/' hosts
-    echo "-----------------------------------------------"
-    shift 
+hosts_file=$1
+sections=$(cat $hosts_file | grep "\[" | tr -d "[]" | sort)
+for i in $sections; do
+    sed -n -e "/\[$i\]/,/\[/p" $hosts_file | sed '${/\[.*/d}'
 done
