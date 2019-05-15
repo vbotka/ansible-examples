7b68ad7070cf13e4e6c94a1bcc23fb2c6183bb4d
Vladimir Botka <vbotka@gmail.com>
2019-05-15 08:44:07 +0200

    Commented example-082

--- examples/example-082/play1.bash
+++ examples/example-082/play1.bash
@@ -4,3 +4,9 @@ sections=$(cat $hosts_file | grep "\[" | tr -d "[]" | sort)
 for i in $sections; do
     sed -n -e "/\[$i\]/,/\[/p" $hosts_file | sed '${/\[.*/d}'
 done
+
+# Is there a way to sort the groups in an ansible host file without
+# sorting the hosts within the groups?
+# https://stackoverflow.com/questions/56034461/is-there-a-way-to-sort-the-groups-in-an-ansible-host-file-without-sorting-the-ho
+
+# EOF
