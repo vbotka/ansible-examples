d799fe6b43e9af547b2e782f958a8eea7b70bb44
Vladimir Botka <vbotka@gmail.com>
2019-06-09 20:17:19 +0200

    Updated example-085/filter_plugins/string_filters.py

--- examples/example-085/filter_plugins/string_filters.py
+++ examples/example-085/filter_plugins/string_filters.py
@@ -2,14 +2,20 @@
 # All rights reserved.
 # Simplified BSD License
 
-def string_prefix(prefix, s):
+def string_prefix(s, prefix):
     return prefix + s
-def string_postfix(postfix, s):
+def string_postfix(s, postfix):
     return s + postfix
+def string_count(s, sub):
+    return s.count(sub)
+def string_find(s, sub):
+    return s.find(sub)
 class FilterModule(object):
-    ''' Ansible filters. Python string operations.'''
+    ''' Ansible filters. Python string methods.'''
     def filters(self):
         return {
+            'string_count' : string_count,
+            'string_find' : string_find,
             'string_prefix' : string_prefix,
             'string_postfix' : string_postfix
         }
