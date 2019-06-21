cb05744178adf27fef9a78a40201e9b66ff016b2
Vladimir Botka <vbotka@gmail.com>
2019-06-22 00:13:19 +0200

    Added example-111/combine[3-5].yml dict

---
+++ examples/example-111/filter_plugins/combine5.py
@@ -0,0 +1,23 @@
+def custom_1(h):
+    return {'key3': sum(h.values())}
+
+def dict_merge(x, y, recursive=False):
+    if recursive:
+        z = dict(list(x.items()) + list(y.items()))
+    else:
+        z = x.copy()
+        z.update(y)
+    return z
+
+def dict_keys(d):
+    return list(d)
+
+
+class FilterModule(object):
+
+    def filters(self):
+        return {
+            'custom_1' : custom_1,
+            'dict_keys' : dict_keys,
+            'dict_merge' : dict_merge
+        }
