e3c305aa4da89f2e952264dbcda340aa1e045d15
Vladimir Botka <vbotka@gmail.com>
2020-09-26 21:52:50 +0200

    Updated example-126/arwrapper1.sh

--- examples/example-126/arwrapper1.sh
+++ examples/example-126/arwrapper1.sh
@@ -4,38 +4,47 @@
 # Simplified BSD License, https://opensource.org/licenses/BSD-2-Clause
 
 version="1.0.0"
-
-# Usage
-
+runner=$HOME/bin/ansible-runner
+project=$PWD/$2
+param=${3:-all.yml}
 usage="$(basename "$0") ver $version
 
 Usage:
-  $(basename "$0")  <cmd> project [param]
+  $(basename "$0") <cmd> project [param]
 
 Where:
-  cmd ....... One of the commands: run, stdout, custom, clean, test
+  cmd ....... One of the commands: run, runid, stdout, custom, clean, test
+  project ... Private data directory. See ansible-runner.
+  param ..... Command specific parameter. See commands.
+
+Commands:
+  run project playbook.yml ......... Run playbook.yml in project
+  runid project playbook.yml ....... The same as run plus display artifact id
+  stdout project id ................ Display project/id/stdout
+  custom project id ................ Display custom stat from project/id/stdout
+  clean project .................... Delete project/artifacts
+  test project playbook.yml ........ Test playbook.yml in project
 
 Examples:
   arwrapper run priv9 t9.yml ....... Run playbook t9.yml in priv9
   arwrapper test priv9 t9.yml ...... Test playbook t9.yml in priv9
   arwrapper clean priv9 ............ Delete pri9/artifacts
-  arwrapper stdout priv9 id ........ Display priv9/id/stdout
-  arwrapper custom priv9 id ........ Display custom stat from priv9/id/stdout\n"
-
-runner=$HOME/bin/ansible-runner
-project=$PWD/$2
-param=${3:-all.yml}
+  arwrapper stdout priv9 id1 ....... Display priv9/artifacts/id1/stdout
+  arwrapper custom priv9 id1........ Display custom stat from stdout\n"
 
 case "$1" in
     test)
 	echo $(date '+%Y-%m-%d %H:%M:%S') $runner run $project -p $param
 	;;
-    run)
+    run|runid)
 	echo $(date '+%Y-%m-%d %H:%M:%S') $0
 	if [ -f "$HOME/.ssh/environment" ]; then
 	    source $HOME/.ssh/environment
 	fi
 	$runner run $project -p $param
+	if [ "$1" = "runid" ]; then
+	    ls -t $project/artifacts | head -1
+	fi
 	;;
     stdout)
 	cat $project/artifacts/$param/stdout
