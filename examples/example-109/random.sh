e4b9a117b119c770ae9d75f5ed70a17047278c43
Vladimir Botka <vbotka@gmail.com>
2021-03-16 00:28:08 +0100

    Add example-109/until-5.yml

---
+++ examples/example-109/random.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+printf "Service Status: "
+[ "$((1 + $RANDOM % 10))" -lt "5" ] && echo up || echo down
+printf "Service Status: "
+[ "$((1 + $RANDOM % 10))" -lt "5" ] && echo up || echo down
+printf "Service Status: "
+[ "$((1 + $RANDOM % 10))" -lt "5" ] && echo up || echo down
