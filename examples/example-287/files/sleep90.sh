866603a707772e385065218326c9246103aff9a1
Vladimir Botka <vbotka@gmail.com>
2022-08-16 03:43:34 +0200

    Fixes and updates. Lint passed.

--- examples/example-287/files/sleep90.sh
+++ examples/example-287/files/sleep90.sh
@@ -1,20 +1,30 @@
 #!/bin/sh
 
-PIDFILE=/var/run/sleep90.pid
-[ -f $PIDFILE ] && exit 0
+timeout=${1:-90}
+counter=${2:-none}
 
-trap 'rm ${PIDFILE}
+basename=${0##*/}
+pidfile=/var/run/${basename%.*}.pid
+[ -f ${pidfile} ] && exit 0
+
+trap 'rm ${pidfile}
 exit 0' SIGINT SIGTERM
 
-echo $$ > ${PIDFILE}
+echo $$ > ${pidfile}
 
 i=1
-while [ $i -lt 90 ]
+if [ ${counter} == 'counter' ]; then
+    echo ${i}
+fi
+while [ ${i} -lt ${timeout} ]
 do
     sleep 1
     i=$(($i+1))
+    if [ ${counter} == 'counter' ]; then
+	echo -e '\e[1A\e[K'${i}
+    fi
 done
 
-cat ${PIDFILE}
-rm ${PIDFILE}
+cat ${pidfile}
+rm ${pidfile}
 exit 0
